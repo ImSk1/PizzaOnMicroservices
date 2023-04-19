1d16c3d4aca56e66ec40724188bbdc6d730ccd03
David Hristov <orionvt3@gmail.com>
2023-04-19 20:39:18 +0300

    init identity microservice

---
+++ src/Services/Identity/New folder/Identity.API/Identity.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Identity.API.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+
+    }
+}
---
+++ src/Services/Identity/New folder/Identity.API/Identity.API/Extensions/ProgramExtensions.cs
@@ -0,0 +1,89 @@
+﻿using Identity.API.Data;
+using Identity.API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace Identity.API.Extensions
+{
+    public static class ProgramExtensions
+    {
+        public static void AddCustomConfiguration(this WebApplicationBuilder builder)
+        {
+            var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            builder.Configuration.AddConfiguration(configBuilder.Build()).Build();
+
+        }
+        public static void AddCustomApplicationServices(this WebApplicationBuilder builder)
+        {
+           
+        }
+       
+        public static void AddCustomAuthentication(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddAuthentication();
+        }
+        public static void AddCustomMvc(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddControllers();
+            builder.Services.AddRazorPages();
+
+        }
+        public static void AddCustomDatabase(this WebApplicationBuilder builder) =>
+            builder.Services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlServer(builder.Configuration["ConnectionString"]));
+        public static void AddCustomIdentity(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+        }
+        public static Serilog.ILogger CreateSerilogLogger(IConfiguration config)
+        {
+            var appName = "Identity.API";
+
+            var seqServerUrl = config["SeqServerUrl"];
+            var logstashUrl = config["LogstashgUrl"];
+
+            var loggerConfig = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .Enrich.WithProperty("ApplicationContext", appName)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.Seq(string.IsNullOrWhiteSpace(seqServerUrl) ? "http://seq" : seqServerUrl)
+                .WriteTo.Http(string.IsNullOrWhiteSpace(logstashUrl) ? "http://localhost:8080" : logstashUrl, null)
+                .ReadFrom.Configuration(config)
+                .CreateLogger();
+
+            return loggerConfig;
+        }
+        public static void AddCustomIdentityServer(this WebApplicationBuilder builder)
+        {
+                var identityServerBuilder = builder.Services.AddIdentityServer(options =>
+                {
+                    options.IssuerUri = "null";
+                    options.Authentication.CookieLifetime = TimeSpan.FromHours(2);
+
+                    options.Events.RaiseErrorEvents = true;
+                    options.Events.RaiseInformationEvents = true;
+                    options.Events.RaiseFailureEvents = true;
+                    options.Events.RaiseSuccessEvents = true;
+                })
+                //.AddInMemoryIdentityResources(Config.GetResources())
+                //.AddInMemoryApiScopes(Config.GetApiScopes())
+                //.AddInMemoryApiResources(Config.GetApis())
+                //.AddInMemoryClients(Config.GetClients(builder.Configuration))
+                .AddAspNetIdentity<ApplicationUser>();
+
+            // not recommended for production - you need to store your key material somewhere secure
+            identityServerBuilder.AddDeveloperSigningCredential();
+        }
+
+    }
+}
